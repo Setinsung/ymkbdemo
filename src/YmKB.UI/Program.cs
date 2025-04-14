c2ce05bc4dd6da1a8920a0800a3d17f8dd23c805
Setinsung <1968989251@qq.com>
2025-04-15 02:05:39 +0800

    UI基本创建

---
+++ src/YmKB.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Extensions;
+using YmKB.UI;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServicesWithExtensions();
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
