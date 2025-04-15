b81340d80e5d20164b49a5c1f74a667cf63c9b34
Setinsung <1968989251@qq.com>
2025-04-15 15:51:26 +0800

    Domain测试实体类

---
+++ src/YmKB.Domain.Entities/Trial/Product.cs
@@ -0,0 +1,83 @@
+﻿using YmKB.Domain.Abstractions.Common;
+
+namespace YmKB.Domain.Entities.Trial;
+
+public class Product : BaseAuditableEntity, IAuditTrial
+{
+    /// <summary>
+    /// 获取或设置产品的 SKU。
+    /// </summary>
+    public string SKU { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 获取或设置产品的名称。
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 获取或设置产品的类别。
+    /// </summary>
+    public ProductCategory Category { get; set; } = ProductCategory.Electronics;
+
+    /// <summary>
+    /// 获取或设置产品的描述。
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// 获取或设置产品的价格。
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// 获取或设置产品价格的货币。
+    /// </summary>
+    public string? Currency { get; set; }
+
+    /// <summary>
+    /// 获取或设置产品的度量单位。
+    /// </summary>
+    public string? UOM { get; set; }
+}
+
+
+/// <summary>
+/// Represents the category of a product.
+/// </summary>
+public enum ProductCategory
+{
+    /// <summary>
+    /// Electronics category.
+    /// </summary>
+    Electronics,
+
+    /// <summary>
+    /// Furniture category.
+    /// </summary>
+    Furniture,
+
+    /// <summary>
+    /// Clothing category.
+    /// </summary>
+    Clothing,
+
+    /// <summary>
+    /// Food category.
+    /// </summary>
+    Food,
+
+    /// <summary>
+    /// Beverages category.
+    /// </summary>
+    Beverages,
+
+    /// <summary>
+    /// Health care category.
+    /// </summary>
+    HealthCare,
+
+    /// <summary>
+    /// Sports category.
+    /// </summary>
+    Sports,
+}
---
+++ src/YmKB.Domain.Entities/Trial/Stock.cs
@@ -0,0 +1,29 @@
+﻿using YmKB.Domain.Abstractions.Common;
+
+namespace YmKB.Domain.Entities.Trial;
+
+public class Stock : BaseAuditableEntity, IAuditTrial, ISoftDelete
+{
+    /// <summary>
+    /// 获取或设置产品 ID。
+    /// </summary>
+    public string? ProductId { get; set; }
+
+    /// <summary>
+    /// 获取或设置与存储关联的产品。
+    /// </summary>
+    public Product? Product { get; set; }
+
+    /// <summary>
+    /// 获取或设置存储的数量。
+    /// </summary>
+    public int Quantity { get; set; }
+
+    /// <summary>
+    /// 获取或设置存储的位置。
+    /// </summary>
+    public string Location { get; set; } = string.Empty;
+
+    public DateTime? Deleted { get; set; }
+    public string? DeletedBy { get; set; }
+}
