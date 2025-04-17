6111fc6178acfc0923464d1088cbb8657ace17d1
Setinsung <1968989251@qq.com>
2025-04-17 19:12:07 +0800

    简单样式修改

--- src/YmKB.UI/Program.cs
+++ src/YmKB.UI/Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServicesWithExtensions();
 builder.Services.AddBlazoredLocalStorage();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7219") });
 
 await builder.Build().RunAsync();
