5cc11e6957cde7af770bdf6c61cda68d69365a6b
Setinsung <1968989251@qq.com>
2025-05-08 19:50:15 +0800

    file uploads

---
+++ src/YmKB.UI/Extensions/CommonHelpers.cs
@@ -0,0 +1,38 @@
+﻿namespace YmKB.UI.Extensions;
+
+public static class CommonHelpers
+{
+    
+    // 截断文本
+    public static string TruncateText(string? text, int maxLength)
+    {
+        if (string.IsNullOrEmpty(text) || text.Length <= maxLength)
+        {
+            return text ?? "";
+        }
+        return text[..(maxLength - 3)] + "...";
+    }
+
+    // 格式化文件大小
+    public static string FormatFileSize(long bytes)
+    {
+        string[] suffixes = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        int i = 0;
+        double dblSByte = bytes;
+        if (bytes > 1024)
+        {
+            for (i = 0; (bytes / 1024) > 0; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+        }
+        return $"{dblSByte:N2} {suffixes[i]}";
+    }
+    
+    public static string FormatDateTimeOst(DateTimeOffset dto)
+    {
+        DateTimeOffset adjustedDateTime = dto.AddHours(8);
+        string customFormat = adjustedDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        return customFormat;
+    }
+}
---
+++ src/YmKB.UI/Services/JsInterops/ClipboardInterop.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.JSInterop;
+
+namespace YmKB.UI.Services.JsInterops;
+
+public class ClipboardInterop(IJSRuntime jsRuntime)
+{
+    public async Task CopyToClipboard(string text)
+    {
+        await jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+    }
+}
