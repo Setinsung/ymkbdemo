a6d1b40516188c6079b86732babf3e269d4d7d3c
Setinsung <1968989251@qq.com>
2025-05-09 12:08:37 +0800

    retryQuantization api

--- src/YmKB.API/Endpoints/KbDocFileEndpoints.cs
+++ src/YmKB.API/Endpoints/KbDocFileEndpoints.cs
@@ -5,6 +5,7 @@ using YmKB.Application.Features.KbDocFiles.Commands;
 using YmKB.Application.Features.KbDocFiles.DTOs;
 using YmKB.Application.Features.KbDocFiles.Queries;
 using YmKB.Application.Models;
+using YmKB.Domain.Entities;
 
 namespace YmKB.API.Endpoints;
 
@@ -50,30 +51,56 @@ public class KbDocFileEndpoins(ILogger<KbDocFileEndpoins> logger) : IEndpointReg
                     string kbDocFileId,
                     int pageNumber,
                     int pageSize
-                ) => mediator.Send(new KbDocFileVectorsQuery(kbDocFileId, pageNumber, pageSize)))
+                ) => mediator.Send(new KbDocFileVectorsQuery(kbDocFileId, pageNumber, pageSize))
+            )
             .Produces<KbDocFileVectorDto>()
             .ProducesProblem(StatusCodes.Status404NotFound)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-           .WithSummary("Get KbDocFileVectors by KbDocFileId")
-           .WithDescription("Returns the details of a specific KbDocFileVectors by its unique ID.");
+            .WithSummary("Get KbDocFileVectors by KbDocFileId")
+            .WithDescription(
+                "Returns the details of a specific KbDocFileVectors by its unique ID."
+            );
 
-        
-        
         group
             .MapPost(
-                "/", async ([FromServices] IMediator mediator, [FromBody] CreateKbDocFileCommand command) =>
+                "/",
+                async (
+                    [FromServices] IMediator mediator,
+                    [FromBody] CreateKbDocFileCommand command
+                ) =>
                 {
                     var res = await mediator.Send(command);
                     await QuantitativeBackgroundService.AddKbDocFileAsync(res);
                     return res;
-                })
+                }
+            )
             .Produces<KbDocFileDto>(StatusCodes.Status201Created)
             .ProducesValidationProblem(StatusCodes.Status422UnprocessableEntity)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .ProducesProblem(StatusCodes.Status500InternalServerError)
             .WithSummary("Create a new KbDocFile")
             .WithDescription("Creates a new KbDocFile with the provided details.");
+
+        group
+            .MapPost(
+                "/retryQuantization/{id}",
+                async ([FromServices] IMediator mediator, [FromRoute] string id) =>
+                {
+                    var kbDocFileDto = await mediator.Send(new GetKbDocFileByIdQuery(id));
+                    if (kbDocFileDto == null)
+                        throw new KeyNotFoundException("文档不存在");
+                    if (kbDocFileDto.Status == QuantizationState.Accomplish)
+                        return;
+                    await QuantitativeBackgroundService.AddKbDocFileAsync(kbDocFileDto.Id);
+                }
+            )
+            .Produces(StatusCodes.Status200OK)
+            .ProducesValidationProblem(StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithSummary("Retry Quantization")
+            .WithDescription("Retry Quantization");
 
         group
             .MapPut(
