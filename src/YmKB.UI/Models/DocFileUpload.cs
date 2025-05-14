94eef9cb876db7115ecf5943273429f063c52c02
Setinsung <1968989251@qq.com>
2025-05-14 22:46:05 +0800

    整合上传网页和文件，支持不同分段模式

---
+++ src/YmKB.UI/Models/DocFileUpload.cs
@@ -0,0 +1,15 @@
+﻿using MudBlazor.Extensions.Components;
+using YMKB.UI.APIs.Models;
+
+namespace YmKB.UI.Models;
+
+public class DocFileUpload
+{
+    public IList<UploadableFile> UploadRequests { get; set; } = [];
+
+    public string? WebPageUrl { get; set; } = "";
+    public string? WebPageName { get; set; } = "";
+
+    public NullableOfSegmentPattern SegmentPattern { get; set; } = NullableOfSegmentPattern.Subsection;
+
+}
